f57ea63b9c0985ac0f803f8d3b63969c3390b830
JuliCarracedo <julicarra@hotmail.com>
2021-06-04 13:23:31 -0300

    clean master

--- app/models/article.rb
+++
@@ -1,6 +0,0 @@
-class Article < ApplicationRecord
-    has_many :comments
-
-    validates :title, presence: true, length: {minimum: 3}
-    validates :body, presence: true, length: { minimum: 10 }
-  end
